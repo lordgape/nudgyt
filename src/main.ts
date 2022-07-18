a799640149030f54c6c94e010ba6d33314af220a
Chesvic Lordgape <lordgape@gmail.com>
2022-07-18 21:44:44 +0100

    Add Security

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,20 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { useContainer } from 'class-validator';
-
+import helmet from 'helmet';
 import { AppModule } from './app.module';
+import * as compression from 'compression';
 
 /* istanbul ignore file */
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
+  app.use(compression());
   app.useGlobalPipes(new ValidationPipe());
+
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
+
   const PORT = process.env.PORT || 3000;
   await app.listen(PORT, () => {
     Logger.log(`Server is up and running from port ${PORT}`);
